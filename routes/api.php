46dc51377f65320194b7b7f67923e786c604e736
Rafli1709 <muhammadrafli696@gmail.com>
2024-12-10 18:12:05 +0800

    Update API Limiter

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChecklistController;
 use App\Http\Controllers\TodoItemController;
+use App\Http\Controllers\ChecklistController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('checklist', ChecklistController::class);
     Route::apiResource('todo-item', TodoItemController::class);
 });
+
 
 require __DIR__ . '/auth.php';
